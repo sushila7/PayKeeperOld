79304546e71e326bf6153e1a8fde3d6fc0ab21a5
bishram78 <bishram.munda78@gmail.com>
2019-11-01 22:46:21 +0530

    Activity added -> 'AddOwnerRenterActivity.java' with resource file 'activity_add_owner_renter.xml'. User Interface for the 'AddOwnerRenterActivity' designed

---
+++ app/src/main/java/com/bishram/payment/keeper/activities/AddOwnerRenterActivity.java
@@ -0,0 +1,16 @@
+package com.bishram.payment.keeper.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bishram.payment.keeper.R;
+
+public class AddOwnerRenterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_owner_renter);
+    }
+}
--- app/src/main/java/com/bishram/payment/keeper/activities/OwnersRentersListActivity.java
+++ app/src/main/java/com/bishram/payment/keeper/activities/OwnersRentersListActivity.java
@@ -2,15 +2,41 @@ package com.bishram.payment.keeper.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.bishram.payment.keeper.R;
 
-public class OwnersRentersListActivity extends AppCompatActivity {
+public class OwnersRentersListActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonAddORMiddle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owners_renters_list);
+
+        initializeViews();
+
+        setButtonClickListeners();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_or_list_add_new_or_middle:
+                startActivity(new Intent(OwnersRentersListActivity.this, AddOwnerRenterActivity.class));
+                break;
+        }
+    }
+
+    private void initializeViews() {
+        buttonAddORMiddle = findViewById(R.id.button_or_list_add_new_or_middle);
+    }
+
+    private void setButtonClickListeners() {
+        buttonAddORMiddle.setOnClickListener(this);
     }
 }
